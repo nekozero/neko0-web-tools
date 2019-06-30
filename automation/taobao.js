2616c4d98df0e5a930211ea70cb9741919d626d9
JoJunIori <jojuniori@moem.cc>
2019-06-30 08:26:36 +0800

    忘记更新版本号了

--- automation/taobao.js
+++ automation/taobao.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Neko0] 淘宝天猫一键好评
 // @description  淘宝&天猫评价页面添加一键好评按钮
-// @version      1.5.6
+// @version      1.5.7
 // @author       JoJunIori
 // @namespace    neko0-web-tools
 // @homepageURL  https://github.com/nekozero/neko0-web-tools
