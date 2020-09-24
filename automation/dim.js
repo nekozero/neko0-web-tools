7c7097d088fcf7b97001f93d808f59a42c0ce55f
JoJunIori <i@neko0.com>
2020-09-24 08:05:03 +0800

    更新DIM增强

--- automation/dim.js
+++ automation/dim.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Neko0] DIM增强
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://app.destinyitemmanager.com/4611686018489549229/d2/inventory
@@ -9,22 +9,19 @@
 // @run-at       document-idle
 // @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
+
+// 按键
+keyList = ['81','87','69','82']
 // 快捷键加强
 $(document).keydown(function(event) {
     // 操作框
     let actionBox = $('.item-details:last-child>div>div')
     // 当前按键
-    let keyCode = event.keyCode.toString()
-    if (keyCode.match(/81/)) {
-        $(actionBox[0]).children('div:nth-child(2)').children().click()
-    }
-    if (keyCode.match(/87/)) {
-        $(actionBox[1]).children('div:nth-child(2)').children().click()
-    }
-    if (keyCode.match(/69/)) {
-        $(actionBox[2]).children('div:nth-child(2)').children().click()
-    }
-    if (keyCode.match(/82/)) {
-        $(actionBox[3]).children('div:nth-child(1)').children().click()
+    let keyCode = event.keyCode
+    // 匹配
+    for(i in keyList){
+        if (keyCode == keyList[i]) {
+            $(actionBox[i]).children('div:nth-child('+$(actionBox[i]).children().length+')').children().click()
+        }
     }
 })
