7a2d4d2a6bf119e5b0b271d68c9a7e556ea65bb3
JoJunIori <i@neko0.com>
2021-09-28 12:18:20 +0800

    update version

--- automation/taobao/main.js
+++ automation/taobao/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Neko0] 淘宝天猫一键好评
 // @description  用于方便地积攒淘气值，以享用高淘气值的低价88VIP等特殊权益来省钱
-// @version      1.7.3
+// @version      1.7.4
 // @author       JoJunIori
 // @namespace    neko0-web-tools
 // @icon         https://www.taobao.com/favicon.ico
