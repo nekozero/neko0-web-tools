7b6772e9e976a70f80958ef497f469a530015dd4
JoJunIori <i@neko0.com>
2024-11-30 07:00:31 +0800

    更新Gemini安全性设定，从而让更多商品可以被正常生成评论

--- automation/taobao/main.js
+++ automation/taobao/main.js
@@ -258,6 +258,24 @@ async function Gemini(aitext_commit) {
 
 	// Data
 	const data = {
+		safetySettings: [
+			{
+				category: 'HARM_CATEGORY_HATE_SPEECH',
+				threshold: 'BLOCK_NONE',
+			},
+			{
+				category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+				threshold: 'BLOCK_NONE',
+			},
+			{
+				category: 'HARM_CATEGORY_HARASSMENT',
+				threshold: 'BLOCK_NONE',
+			},
+			{
+				category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+				threshold: 'BLOCK_NONE',
+			},
+		],
 		contents: [
 			{
 				role: 'user',
